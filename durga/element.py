aa1bd34275d483f32009979b7f5070d5d0bc527a
Markus Zapke-Gründemann <markus@transcode.de>
2014-12-08 13:27:37 +0100

    Fully implemented Element.get_url()

--- durga/element.py
+++ durga/element.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 
 
 class Element(object):
+    def __init__(self, resource):
+        self._resource = resource
+
     def update(self, data):
         pass
 
@@ -10,11 +13,16 @@ class Element(object):
         pass
 
     def get_url(self):
-        if hasattr(self, 'url'):
-            url = self.url
-        else:
-            resource = self.get_resource()
-            url = '{0}/{1}'.format(resource.get_url(), getattr(self, resource.id_attribute))
+        resource = self.get_resource()
+        url_attribute = getattr(resource, 'url_attribute', 'url')
+        try:
+            url = getattr(self, url_attribute)
+        except AttributeError:
+            id_attribute = getattr(resource, 'id_attribute', None)
+            assert id_attribute, (
+                'You must define an id_attribute attribute at {0}.'.format(resource.__class__.__name__)  # noqa
+            )
+            url = '{0}/{1}'.format(resource.get_url(), getattr(self, id_attribute))
         return url
 
     def get_resource(self):
--- tests/test_element.py
+++ tests/test_element.py
@@ -1,12 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import pytest
+
 from durga import element
 
 
-class TestElement(element.Element):
-    url = 'https://api.example.com/movies/23'
+def test_get_default_url_attribute(resource):
+    class DefaultURLElement(element.Element):
+        url = 'https://api.example.com/movies/23'
+
+    assert DefaultURLElement(resource).get_url() == DefaultURLElement.url
 
 
-def test_get_url_attribute():
-    assert TestElement().get_url() == 'https://api.example.com/movies/23'
+def test_get_custom_url_attribute(resource):
+    resource.url_attribute = 'resource_uri'
+
+    class CustomURLElement(element.Element):
+        _resource = resource
+        resource_uri = 'https://api.example.com/movies/23'
+
+    assert CustomURLElement(resource).get_url() == CustomURLElement.resource_uri
+
+
+def test_get_dynamic_url(resource):
+    class DynamicURLElement(element.Element):
+        _resource = resource
+        id = '23'
+
+    with pytest.raises(AssertionError) as excinfo:
+        DynamicURLElement(None).get_url()
+    assert excinfo.value.msg == 'You must define an id_attribute attribute at NoneType.'
+    resource.id_attribute = 'id'
+    element_obj = DynamicURLElement(resource)
+    assert element_obj.get_url() == '/'.join([resource.get_url(), element_obj.id])
