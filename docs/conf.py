356bdbc02584374992e1b997cbafda82302837a6
Markus Zapke-Gründemann <markus@transcode.de>
2014-12-09 11:44:36 +0100

    Use os.path.dirname() in favor of os.getcwd() to determine project root

--- docs/conf.py
+++ docs/conf.py
@@ -23,8 +23,7 @@ import os
 #sys.path.insert(0, os.path.abspath('.'))
 
 # Get the project root dir, which is the parent dir of this
-cwd = os.getcwd()
-project_root = os.path.dirname(cwd)
+project_root = os.path.dirname(os.path.dirname(__file__))
 
 # Insert the project root dir as the first element in the PYTHONPATH.
 # This lets us ensure that the source package is imported, and that its
