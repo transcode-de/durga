e3cb8c15ae0b9a299cfa81fb73bf349c6db76d1c
Markus Zapke-Gründemann <markus@transcode.de>
2014-12-09 11:46:50 +0100

    Updated flickr integration test to use REST API

--- tests/test_flickr.py
+++ tests/test_flickr.py
@@ -10,10 +10,10 @@ from durga.element import Element
 basestring = six.string_types[0]
 
 
-class FlickrPhotoSearchResource(durga.Resource):
-    base_url = 'https://query.yahooapis.com/v1/public'
-    name = 'yql'
-    results_path = ('query', 'results', 'photo')
+class FlickrResource(durga.Resource):
+    base_url = 'https://api.flickr.com/services'
+    name = 'rest'
+    results_path = ('photos', 'photo')
     schema = durga.schema.Schema({
         'farm': durga.schema.Use(int),
         'id': durga.schema.Use(int),
@@ -25,19 +25,20 @@ class FlickrPhotoSearchResource(durga.Resource):
         'server': durga.schema.Use(int),
         'title': durga.schema.And(basestring, len),
     })
+    query = {
+        'method': 'flickr.photos.search',
+        'api_key': 'a33076a7ae214c0d12931ae8e38e846d',
+        'format': 'json',
+        'nojsoncallback': 1,
+    }
 
 
 @pytest.mark.integrationtest
 def test_flickr():
-    flickr = FlickrPhotoSearchResource()
-    query = {
-        'q': 'select * from flickr.photos.search where text="Cat" and api_key="92bd0de55a63046155c09f1a06876875"',  # noqa
-        'format': 'json'
-    }
-    images = flickr.collection.filter(**query)
+    images = FlickrResource().collection.filter(text='Cat', per_page=10)
     assert images.count() == 10
     image = images[0]
-    assert image.__class__.__name__.lower().startswith(flickr.name)
+    assert image.__class__.__name__.lower().startswith(FlickrResource.name)
     assert isinstance(image, Element)
     assert isinstance(image.id, int)
     assert isinstance(image.isfamily, bool)
