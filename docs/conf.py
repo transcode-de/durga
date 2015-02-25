5830e319e85b558e6fa7c24597e11d2edec163b2
Markus Zapke-Gründemann <markus@transcode.de>
2015-02-25 16:43:26 +0100

    Updated Sphinx configuration for 1.3

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ import durga
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
@@ -110,7 +110,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
