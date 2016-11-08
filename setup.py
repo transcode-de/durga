f6afdbc57f892a7a1cdc78af75cf2d24e019bd9e
requires.io <support@requires.io>
2016-11-08 19:07:02 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ tests_requires = [
     'httpretty>=0.8.0,!=0.8.1,!=0.8.2,!=0.8.3,!=0.8.7,!=0.8.8,!=0.8.9',
     'pytest-httpretty==0.2.0',
     'pytest==3.0.3',
-    'python-dateutil==2.5.3',
+    'python-dateutil==2.6.0',
     'six==1.10.0',
     'tox==2.4.1',
     'virtualenv>=12.0.2,!=12.0.4,!=12.0.5,!=12.0.6,!=12.0.7',
